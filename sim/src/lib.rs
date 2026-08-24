7946bb42046aeff2440b09441433d5bbfef166a2
wanickols <wanickols@ualr.edu>
2026-08-23 21:43:32 -0600

    Add Bevy, seperate project into visualization and simulation

---
+++ sim/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod sim;
+
+//TODO, add simulation
+//I want a simulation class, then main can just use that
+//then bevy can grab simulation, and probalby an exposed world
+//then it moves! :)
---
+++ visualizer/src/main.rs
@@ -0,0 +1,42 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Ground
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(20.0, 20.0))),
+        MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
+    ));
+
+    // Our extremely advanced drone
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 0.25, 1.0))),
+        MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
+        Transform::from_xyz(0.0, 2.0, 0.0),
+    ));
+
+    // Light
+    commands.spawn((
+        PointLight {
+            shadow_maps_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(4.0, 8.0, 4.0),
+    ));
+
+    // Camera
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(5.0, 5.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+}
